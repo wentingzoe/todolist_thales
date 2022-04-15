d1d6e7c69f16ac127fa1822eb9b7b9f49b046eda
wentingzoe <ywtzoe@gmail.com>
2022-04-15 17:05:14 -0400

    create todolist for thales

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+
+import '../styles/App.css';
+import React from "react";
+import TodoLists from './TodoLists';
+
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>Thales To Do List</h1>
+      <TodoLists />
+    </div>
+  );
+}
+
+
+
+export default App;
